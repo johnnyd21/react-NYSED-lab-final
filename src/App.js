cd007a20adb4d461ab029d5d2b15c6a06457c568
johnnyd21 <jdominguez@richmondhillhs.org>
2020-10-17 16:14:16 +0000

    Updated Lab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
-// Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+import SchoolCard from './components/SchoolCard.js';
+import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
-  return (
+    return (
     <div className="App">
       <Navbar/>
       <div className="container">
-        <div className="row">
+        <div className="row" >
+        <SchoolCard school={data[0]} />
+        <SchoolCard school={data[1]} />
+        <SchoolCard school={data[2]} />
+        <SchoolCard school={data[3]} />
+        <SchoolCard school={data[4]} />
+        <SchoolCard school={data[5]} />
+            
 
         </div>
       </div>
